6fa12a890e65e9b1725e4e58683006e21a84b841
Jeferson Farias Calazans <calazans10@gmail.com>
2015-01-20 12:47:48 -0200

    Update active_file.rb

--- lib/active_file.rb
+++ lib/active_file.rb
@@ -1,5 +1,3 @@
-# coding utf-8
-
 require "yaml"
 require "fileutils"
 require "active_file/version"
@@ -8,94 +6,109 @@ require "rake"
 import File.expand_path("../tasks/db.rake", __FILE__)
 
 module ActiveFile
-    def save
-        @new_record = false # seta objeto como registro existente
+  def save
+    @new_record = false
 
-        File.open("db/revistas/#{@id}.yml", "w") do |file|
-            file.puts serialize
+    File.open("db/revistas/#{@id}.yml", "w") do |file|
+      file.puts serialize
+    end
+  end
+
+  def destroy
+    unless @destroyed or @new_record
+      @destroyed = true
+      FileUtils.rm "db/revistas/#{@id}.yml"
+    end
+  end
+
+  module ClassMethods
+    def field(name)
+      @fields ||= []
+      @fields << name
+
+      get = %Q{
+        def #{name}
+          @#{name}
         end
+      }
+
+      set = %Q{
+        def #{name}=(valor)
+          @#{name}=valor
+        end
+      }
+
+      self.class_eval get
+      self.class_eval set
     end
 
-    def destroy(id)
-        unless @destroyed or @new_record
-            @destroyed = true
-            FileUtils.rm "db/revistas/#{@id}.yml"
-        end
+    def find(id)
+      unless File.exists?("db/revistas/#{id}.yml")
+        raise DocumentNotFound, "Arquivo db/revistas/#{id}.yml não encontrado.", caller
+      end
+
+      deserialize "db/revistas/#{id}.yml"
     end
 
-    module ClassMethods
-      def field(name, required: false, default: "")
-        @fields ||= []
-        @fields << Field.new(name, required, default)
-
-        self.class_eval %Q$
-            attr_accessor *#{@fields.map(&:name)}
-            attr_reader :id, :destroyed, :new_record
-
-            def initialize(#{@fields.map(&:to_argument).join(", ")})
-                @id = self.class.next_id
-                @destroyed = false
-                @new_record = true
-                #{@fields.map(&:to_assign).join("\n")}
-            end
-        $
-      end
-
-      def find(id)
-        raise DocumentNotFound, "Arquivo db/revistas/#{id} nao encontrado.", caller unless File.exists?("db/revistas/#{id}.yml")
-        deserialize "db/revistas/#{id}.yml"
-      end
-
-      def next_id
-          Dir.glob("db/revistas/*.yml").size + 1
-      end
-
-      def method_missing(name, *args, &block)
-        field = name.to_s.split("_").last
-        super if @fields.map(&:name).include? field
-
-        load_all.select do |object|
-          object.send(field) == args.first
-        end
-      end
-
-      private
-
-      def load_all
-        Dir.glob('db/revistas/*.yml').map do |file|
-          deserialize file
-        end
-      end
-
-      def deserialize(file)
-        YAML.load File.open(file, "r")
-      end
+    def next_id
+      Dir.glob("db/revistas/*.yml").size + 1
     end
 
-    class Field
-      attr_reader :name, :required, :default
+    def method_missing(name, *args, &block)
+      super unless name.to_s =~ /^find_by_(.*)/
 
-      def initialize(name, required, default)
-        @name, @required, @default = name, required, default
+      argument = args.first
+      field = $1
+      super unless @fields.include? field.to_sym
+
+      load_all.select do |object|
+        should_select? object, field, argument
       end
-
-      def to_argument
-        "#{@name}: #{@default}"
-      end
-
-      def to_assign
-        "@#{@name} = #{@name}"
-      end
-    end
-
-    def self.included(base)
-        base.extend ClassMethods
     end
 
     private
 
-    def serialize
-        YAML.dump self
+    def should_select?(object, field, argument)
+      if argument.kind_of? Regexp
+        object.send(field) =~ argument
+      else
+        object.send(field) == argument
+      end
     end
+
+    def load_all
+      Dir.glob('db/revistas/*.yml').map do |file|
+        deserialize file
+      end
+    end
+
+    def deserialize(file)
+      YAML.load File.open(file, "r")
+    end
+  end
+
+  def self.included(receiver)
+    receiver.extend ClassMethods
+
+    receiver.class_eval do
+      attr_reader :id, :destroyed, :new_record
+
+      def initialize(params = {})
+        @id = self.class.next_id
+        @destroyed = false
+        @new_record = true
+
+        params.each do |key, value|
+          instance_variable_set "@#{key}", value
+        end
+      end
+    end
+  end
+
+  private
+
+  def serialize
+    YAML.dump self
+  end
 end
 
